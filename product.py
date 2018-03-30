6c9e427f607d8f14f9a83261bb0546e4f71c1fbb
Ethan Furman <ethan@stoneleaf.us>
2018-03-29 19:33:28 -0700

    remove fis_qty_available as it's now in fis_integration

--- product.py
+++ product.py
@@ -9,38 +9,24 @@ class product_product(osv.Model):
     _name = 'product.product'
     _inherit = 'product.product'
 
-    def _calc_avail_qtys(self, cr, uid, ids, field_names, args, context=None):
+    def _calc_makeable(self, cr, uid, ids, field_name, args, context=None):
+        # XXX should this use the by-order recipe method instead of the
+        #     by-item recipe method?
         if isinstance(ids, (int, long)):
             ids = [ids]
         res = {}
-        if not field_names:
+        if field_name != 'fis_qty_makeable':
             return res
-        datas = self.read(
-            cr, uid, ids,
-            fields=[
-                'id', 'xml_id',
-                'fis_qty_on_hand', 'fis_qty_produced', 'fis_qty_consumed',
-                'fis_qty_purchased', 'fis_qty_sold',
-                ],
-            context=context)
+        datas = self.read(cr, uid, ids, fields=['id', 'xml_id'], context=context)
         for row in datas:
             values = res[row['id']] = {}
-            if 'fis_qty_available' in field_names:
-                values['fis_qty_available'] = sum([
-                        row['fis_qty_on_hand'],
-                        row['fis_qty_produced'],
-                        row['fis_qty_consumed'],
-                        row['fis_qty_purchased'],
-                        row['fis_qty_sold'],
-                        ])
-            if 'fis_qty_makeable' in field_names:
-                values['fis_qty_makeable'] = recipe.make_on_hand(row['xml_id'])
+            values['fis_qty_makeable'] = recipe.make_on_hand(row['xml_id'])
         return res
 
     def _get_qty_update_ids(self, cr, uid, changed_ids, context=None):
         if isinstance(changed_ids, (int, long)):
             changed_ids = [changed_ids]
-        ids = list(changed_ids)
+        ids = []
         datas = self.read(cr, uid, changed_ids, fields=['prod_ingredient_ids'], context=context)
         for row in datas:
             ids.extend(row['prod_ingredient_ids'])
@@ -54,6 +40,8 @@ class product_product(osv.Model):
             domain=[('state','not in',['complete','cancelled'])],
             order='schedule_date, sequence',
             ),
+        # XXX below only tracks active order ingredients -- should we also track non-active
+        #     order ingrediens?
         'prod_ingredient_ids': fields.one2many(
             'fnx.pd.ingredient',
             'item_id',
@@ -61,31 +49,12 @@ class product_product(osv.Model):
             domain=[('order_state','not in',['complete','cancelled'])],
             ),
         'fis_qty_makeable': fields.function(
-            _calc_avail_qtys,
+            _calc_makeable,
             type='float',
             string='Immediately Producible',
             help="How much can be made with current inventory.",
-            multi='calc_available',
             store={
-                'product.product': (
-                    _get_qty_update_ids,
-                    ['fis_qty_produced', 'fis_qty_consumed', 'fis_qty_purchased', 'fis_qty_sold'],
-                    10,
-                    ),
-                },
-            ),
-        'fis_qty_available': fields.function(
-            _calc_avail_qtys,
-            type='float',
-            string='Quantity Available Today',
-            help="How much inventory is available at this moment.",
-            multi='calc_available',
-            store={
-                'product.product': (
-                    _get_qty_update_ids,
-                    ['fis_qty_produced', 'fis_qty_consumed', 'fis_qty_purchased', 'fis_qty_sold'],
-                    10,
-                    ),
+                'product.product': ( _get_qty_update_ids, ['fis_qty_available'], 10,),
                 },
             ),
         }
