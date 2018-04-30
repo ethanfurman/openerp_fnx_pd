c2ed4bf019161b023386cb35c4e1c97dc91b8c8a
Ethan Furman <ethan@stoneleaf.us>
2018-04-29 21:06:50 -0700

    many fixes

--- scripts/updates_mapping.py
+++ scripts/updates_mapping.py
@@ -1,6 +1,6 @@
 {
-    '': 'production',
-    'IFMS': 'production',
-    'IFDT': 'production',
-    'IFPP': 'production',
+    '': 'fnx_pd',
+    'IFMS': 'fnx_pd',
+    'IFDT': 'fnx_pd',
+    'IFPP': 'fnx_pd',
     }
